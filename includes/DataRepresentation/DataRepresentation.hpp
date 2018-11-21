11a0ebe5dfc86938a0c5964ace275aca104dad09
Viguier Benjamin <benjamin.viguier@epitech.eu>
2018-11-21 19:23:09 +0100

    FIx data rep

--- includes/DataRepresentation/DataRepresentation.hpp
+++ includes/DataRepresentation/DataRepresentation.hpp
@@ -23,12 +23,14 @@ using makeArray = std::initializer_list<ArrayEntry>;
 
 class DataRepresentation
 {
-public:
-	enum CloneOption {
+  public:
+	enum CloneOption
+	{
 		LAZY,
 		DEEP
 	};
-	enum Type: std::uint8_t {
+	enum Type : std::uint8_t
+	{
 		NBR,
 		STR,
 		BUF,
@@ -37,7 +39,8 @@ public:
 		BOL,
 		NUL
 	};
-public:
+
+  public:
 	inline DataRepresentation(DataRepresentation const &) = default;
 	inline DataRepresentation(Type type = NUL);
 	inline DataRepresentation(double nbr);
@@ -50,172 +53,177 @@ public:
 	inline DataRepresentation(std::initializer_list<DataRepresentation> list);
 	inline DataRepresentation(std::initializer_list<ObjEntry> list);
 
-	inline DataRepresentation	&operator=(DataRepresentation const &other) = default;
-	inline bool			operator==(DataRepresentation const &other) const;
-	inline bool			operator!=(DataRepresentation const &other) const;
+	inline DataRepresentation &operator=(DataRepresentation const &other) = default;
+	inline bool operator==(DataRepresentation const &other) const;
+	inline bool operator!=(DataRepresentation const &other) const;
 
-	inline DataRepresentation	&operator[](std::string const &key);
-	inline DataRepresentation	&operator[](unsigned idx);
-	inline DataRepresentation	&push(DataRepresentation const &obj);
-	
-	inline DataRepresentation	clone(CloneOption attr = CloneOption::DEEP) const;
+	inline DataRepresentation &operator[](std::string const &key);
+	inline DataRepresentation &operator[](unsigned idx);
+	inline DataRepresentation &push(DataRepresentation const &obj);
 
-	inline bool	isObject(void) const;
-	inline bool	isArray(void) const;
-	inline bool	isNumber(void) const;
-	inline bool	isString(void) const;
-	inline bool	isBool(void) const;
-	inline bool	isNull(void) const;
-	
-	template<typename T>
-	inline T	&getData();
+	inline DataRepresentation clone(CloneOption attr = CloneOption::DEEP) const;
 
-	template<typename T>
-	inline const T	&getData() const;
+	inline bool isObject(void) const;
+	inline bool isArray(void) const;
+	inline bool isNumber(void) const;
+	inline bool isString(void) const;
+	inline bool isBool(void) const;
+	inline bool isNull(void) const;
 
-	template<typename T>
-	inline auto	&value();
+	template <typename T>
+	inline T &getData();
 
-	template<typename T>
-	inline const auto	&value() const;
+	template <typename T>
+	inline const T &getData() const;
 
-	template<typename T, typename U>
-	inline U	value();
+	template <typename T>
+	inline auto &value();
 
-	template<typename T, typename U>
-	inline U	value() const;
+	template <typename T>
+	inline const auto &value() const;
 
-	static inline DataRepresentation	newObject()
+	template <typename T, typename U>
+	inline U value();
+
+	template <typename T, typename U>
+	inline U value() const;
+
+	static inline DataRepresentation newObject()
 	{
 		return DataRepresentation(OBJ);
 	}
 
-	static inline DataRepresentation	newArray()
+	static inline DataRepresentation newArray()
 	{
 		return DataRepresentation(ARR);
 	}
-private:
-	inline DataRepresentation(std::shared_ptr<laldb::AbstractData> &data);
-private:
-	std::shared_ptr<AbstractData>	_data;
-};
-inline std::ostream	&operator<<(std::ostream &to, DataRepresentation const &me);
 
+  private:
+	inline DataRepresentation(std::shared_ptr<laldb::AbstractData> &data);
+
+  private:
+	std::shared_ptr<AbstractData> _data;
+};
+inline std::ostream &operator<<(std::ostream &to, DataRepresentation const &me);
 
 class AbstractData
 {
-public:
-	virtual bool		operator==(AbstractData const &) const = 0;
-	virtual DataRepresentation::Type	getType() const = 0;
+  public:
+	virtual bool operator==(AbstractData const &) const = 0;
+	virtual DataRepresentation::Type getType() const = 0;
 	virtual std::shared_ptr<AbstractData>
 	clone(DataRepresentation::CloneOption attr) const = 0;
 };
 
 class Number final : public AbstractData
 {
-public:
+  public:
 	inline Number(double nbr);
-	
-	inline virtual bool		operator==(AbstractData const &) const final;
-	inline virtual DataRepresentation::Type	getType() const final;
+
+	inline virtual bool operator==(AbstractData const &) const final;
+	inline virtual DataRepresentation::Type getType() const final;
 	inline virtual std::shared_ptr<AbstractData>
 	clone(DataRepresentation::CloneOption attr) const final;
-	inline double					get(void) const;
-	inline void						set(double val);
-private:
-	double	_value;
+	inline double get(void) const;
+	inline void set(double val);
+
+  private:
+	double _value;
 };
 
 class Buffer final : public AbstractData
 {
-public:
+  public:
 	inline Buffer();
 	inline Buffer(void const *s, std::size_t len);
 
-	inline virtual bool		operator==(AbstractData const &) const final;
-	inline virtual DataRepresentation::Type	getType() const final;
+	inline virtual bool operator==(AbstractData const &) const final;
+	inline virtual DataRepresentation::Type getType() const final;
 	inline virtual std::shared_ptr<AbstractData>
 	clone(DataRepresentation::CloneOption attr) const final;
-	inline void		*get(void);
-	inline const void	*get(void) const;
-	inline std::size_t	size(void) const;
-	inline void			set(const char *buf, std::size_t len);
-private:
-	void		*_data;
-	std::size_t	_len;
+	inline void *get(void);
+	inline const void *get(void) const;
+	inline std::size_t size(void) const;
+	inline void set(const char *buf, std::size_t len);
+
+  private:
+	void *_data;
+	std::size_t _len;
 };
 
 class Null final : public AbstractData
 {
-public:
+  public:
 	inline Null();
-	
-	inline virtual bool		operator==(AbstractData const &) const final;
-	inline virtual DataRepresentation::Type	getType() const final;
+
+	inline virtual bool operator==(AbstractData const &) const final;
+	inline virtual DataRepresentation::Type getType() const final;
 	inline virtual std::shared_ptr<AbstractData>
 	clone(DataRepresentation::CloneOption attr) const final;
-	inline void	*get(void) const;
+	inline void *get(void) const;
 };
 
 class String final : public AbstractData, private std::string
 {
-public:
+  public:
 	inline String(std::string const &str);
 
-	inline virtual bool		operator==(AbstractData const &) const final;
-	inline virtual DataRepresentation::Type	getType() const final;
+	inline virtual bool operator==(AbstractData const &) const final;
+	inline virtual DataRepresentation::Type getType() const final;
 	inline virtual std::shared_ptr<AbstractData>
 	clone(DataRepresentation::CloneOption attr) const final;
 	inline const std::string
-			&get(void) const;
-	inline void	set(std::string const &str);
+		&
+		get(void) const;
+	inline void set(std::string const &str);
 };
 
 class Bool final : public AbstractData
 {
-public:
+  public:
 	inline Bool(bool val);
-	
-	inline virtual bool		operator==(AbstractData const &) const final;
-	inline virtual DataRepresentation::Type	getType() const final;
+
+	inline virtual bool operator==(AbstractData const &) const final;
+	inline virtual DataRepresentation::Type getType() const final;
 	inline virtual std::shared_ptr<AbstractData>
 	clone(DataRepresentation::CloneOption attr) const final;
-	inline bool	get(void) const;
-	inline void	set(bool bol);
-private:
-	bool	_value;
+	inline bool get(void) const;
+	inline void set(bool bol);
+
+  private:
+	bool _value;
 };
 
 class Object final : public AbstractData, private std::unordered_map<std::string, DataRepresentation>
 {
-public:
+  public:
 	inline Object();
 	inline Object(std::initializer_list<ObjEntry> list);
 
-	inline virtual bool		operator==(AbstractData const &) const final;
-	inline virtual DataRepresentation::Type	getType() const final;
+	inline virtual bool operator==(AbstractData const &) const final;
+	inline virtual DataRepresentation::Type getType() const final;
 	inline virtual std::shared_ptr<AbstractData>
 	clone(DataRepresentation::CloneOption attr) const final;
 
-	inline std::unordered_map<std::string, DataRepresentation>	&get(void);
-	inline std::unordered_map<std::string, DataRepresentation>	&get(void) const;
+	inline std::unordered_map<std::string, DataRepresentation> &get(void);
+	inline std::unordered_map<std::string, DataRepresentation> &get(void) const;
 };
 
 class Array final : public AbstractData, private std::vector<DataRepresentation>
 {
-public:
+  public:
 	inline Array();
 	inline Array(std::initializer_list<ArrayEntry> list);
 
-	inline virtual bool		operator==(AbstractData const &) const final;
-	inline virtual DataRepresentation::Type	getType(void) const final;
+	inline virtual bool operator==(AbstractData const &) const final;
+	inline virtual DataRepresentation::Type getType(void) const final;
 	inline virtual std::shared_ptr<AbstractData>
 	clone(DataRepresentation::CloneOption attr) const final;
-	inline std::vector<DataRepresentation>		&get(void);
-	inline std::vector<DataRepresentation>		&get(void) const;
+	inline std::vector<DataRepresentation> &get(void);
+	inline std::vector<DataRepresentation> &get(void) const;
 };
 
-}
+} // namespace laldb
 
 #include "DataRepresentation.tpp"
 
